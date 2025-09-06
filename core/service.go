5d656e5083bb7cf02f2d11d6ceb37aae8d0d6625
ckalgara <chandhukalagara@gmail.com>
2025-09-06 01:37:49 -0500

    chore: inventory lib, health to ping inventory

--- core/service.go
+++ core/service.go
@@ -5,7 +5,14 @@ import (
 	"log"
 	"time"
 
+	pbInventory "github.com/ckalagara/group-a-inventory/proto"
 	pb "github.com/ckalagara/group-a-manager/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	inventoryUrl = "group-a-inventory:50052"
 )
 
 type ServerImpl struct {
@@ -14,5 +21,26 @@ type ServerImpl struct {
 
 func (s *ServerImpl) Health(ctx context.Context, req *pb.HealthRequest) (*pb.HealthResponse, error) {
 	log.Printf("Received health request at %v", time.Now())
+	// Ping inventory health endpoint
+	dailOpts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	c, err := grpc.NewClient(inventoryUrl, dailOpts...)
+	defer c.Close()
+	if err != nil {
+		log.Printf("Could not connect to inventory service: %v", err)
+		return &pb.HealthResponse{Status: "Down"}, nil
+	}
+
+	invClient := pbInventory.NewServiceClient(c)
+	pbRes, err := invClient.Health(ctx, &pbInventory.HealthRequest{})
+	if err != nil {
+		log.Printf("Inventory service health check failed: %v", err)
+		return &pb.HealthResponse{Status: "Down"}, nil
+	}
+	log.Println("Inventory service is responding with status:", pbRes.Status)
+
+	if pbRes.Status != "Service is healthy" {
+		return &pb.HealthResponse{Status: "Down"}, nil
+	}
+
 	return &pb.HealthResponse{Status: "Up"}, nil
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/ckalagara/group-a-manager
 go 1.25.1
 
 require (
+	github.com/ckalagara/group-a-inventory v0.0.1-a1
 	google.golang.org/grpc v1.75.0
 	google.golang.org/protobuf v1.36.8
 )
 
 require (
-	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250826171959-ef028d996bc1 // indirect
 )
